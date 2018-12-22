dcd71ad49a66c31459a082701b8cad91c0952a1e
Susan Hern <freeublue@gmail.com>
2018-12-22 07:07:03 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = "us-cdbr-iron-east-01.cleardb.net";
+$dbname = "heroku_f18400553b651d5";
+$dbuser = "bdeba2f1afd052";
+$dbpass = "01e0bdf6";
+  
+         
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+   
+?>
